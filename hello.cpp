eb44f9d40012f48a64ed0b57f770e06bab8e3c6f
PhumipatJumthong <139094228+PhumipatJumthong@users.noreply.github.com>
2023-07-10 10:35:57 +0700

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{   cout << "Student Code : 6606021620171"<<endl;
+    cout << "Student Name : Phuthong Jumthong"<<endl;
+	cout << "    Division : Information Technology"<<endl;
+	cout << "        Year : 1"<<endl;
+	cout << "        Room : RC"<<endl;
+	cout << "         sex : Man"<<endl;
+	cout << "         Age : 18"<<endl;
+    system("pause");
+	return(0);
+}
