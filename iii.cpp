4c4e4f0ee76aa0567d2d4dff83fa9072d9b1ca55
PhumipatJumthong <139094228+PhumipatJumthong@users.noreply.github.com>
2023-07-17 11:04:11 +0700

    years

---
+++ iii.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{  
+	string Name;
+	short years;
+	cout << "Enter Name :";
+	cin  >>  Name;
+	cout << "Enter Year of birth :";
+	cin  >>  years;
+	cout << "\nYour Age " << 2566+years << "years old"<<endl;
+    return (0);
+}
